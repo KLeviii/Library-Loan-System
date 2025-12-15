01dff86625998152da9d080d2fef57cb6fb3bc40
KLeviii <leventekaldor@gmail.com>
2025-12-15 17:38:07 +0100

    Started work on MenuUI.java

--- src/main/ui/MenuUI.java
+++ src/main/ui/MenuUI.java
@@ -1,6 +1,17 @@
 package main.ui;
 
+import main.service.*;
+import static main.ui.AnsiColors.*;
+
+import java.util.Scanner;
+
 public class MenuUI {
+    private Scanner scanner;
+    private BookService bookService;
+    private MemberService memberService;
+    private LoanService loanService;
+
+    // TODO: constructor and ansi call methods
 
     public void println(String value) {
         System.out.println(value);
